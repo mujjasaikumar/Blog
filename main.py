1c5bf8579ba073e32725c2bb1c376852c2cf7421
mujjasaikumar <mujjasaikumar@gmail.com>
2023-03-18 18:32:53 +0530

    Added PostgreSQL

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from flask_gravatar import Gravatar
 from functools import wraps
 import os
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
