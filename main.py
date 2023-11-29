08be9d05b1a32fc78a508e838ae9066931ad54f2
Saikumar Mujja <95629853+mujjasaikumar@users.noreply.github.com>
2023-11-29 12:38:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
                 return self.application
 
         gunicorn_options = {
-            'bind': '0.0.0.0:8000',  # Specify your desired host and port
+            'bind': '0.0.0.0',  # Specify your desired host and port
             'workers': 3,  # Adjust based on your server's capacity
             'timeout': 30,  # Set request timeout
         }
