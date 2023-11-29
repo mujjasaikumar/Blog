a77cb3c661cc25241de09893fda564308eb7f3ee
Saikumar Mujja <95629853+mujjasaikumar@users.noreply.github.com>
2023-11-29 11:41:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -277,5 +277,5 @@ def delete_comment(comment_id, current_post):
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
 
